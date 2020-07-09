cb0b7bd6a0edcc479489cf0513c230db6f5aeba3
bartek 'basz' szurgot <git@baszerr.eu>
2020-07-09 21:27:45 +0200

    led blinking equivalnet for start

--- board/src/main.cpp
+++ board/src/main.cpp
@@ -1,13 +1,21 @@
 #include "config.hpp"       // this file must be included as first one!
 #include "Watchdog.hpp"
+#include <util/delay.h>
 
 int main(void)
 {
-  Watchdog wdt;
+  //Watchdog wdt;
+  DDRD |= 0xff;
 
   while(true)
   {
-    wdt.reset();
+    PORTD &= 0x00;
+    _delay_ms(1500);
+
+    PORTD |= 0xff;
+    _delay_ms(1500);
+
+    //wdt.reset();
     // TODO
   }
 }

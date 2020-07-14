763b44bc44b7377d71db9e1071295a9eea09c36c
bartek 'basz' szurgot <git@baszerr.eu>
2020-07-14 23:51:02 +0200

    added possiblity to abort program with any button pressed

--- board/src/execute_program.hpp
+++ board/src/execute_program.hpp
@@ -20,46 +20,51 @@ inline void inter_dir_delay(Watchdog& wdg)
 inline void step_delay() { _delay_ms(30); }
 
 template<typename FL, typename FR>
-void execute_action(Watchdog& wdg, FL&& left, FR&& right, const uint16_t steps)
+auto execute_action(Watchdog& wdg, FL&& left, FR&& right, const uint16_t steps)
 {
   for(auto i=0l; i<steps; ++i)
   {
     left();
     right();
     step_delay();
+    // TODO: disable engines here!
     wdg.reset();
+    if( Buttons::any_pressed() )
+      return false;
   }
+  return true;
 }
 
 template<typename FL, typename FR>
-void execute_turn(Watchdog& wdg, FL&& left, FR&& right)
+auto execute_turn(Watchdog& wdg, FL&& left, FR&& right)
 {
   constexpr auto steps_per_turn = 30l;
-  execute_action(wdg, left, right, steps_per_turn);
+  return execute_action(wdg, left, right, steps_per_turn);
 }
 
 template<typename FL, typename FR>
-void execute_drive(Watchdog& wdg, FL&& left, FR&& right)
+auto execute_drive(Watchdog& wdg, FL&& left, FR&& right)
 {
   constexpr auto steps_per_drive_len = 100l;
-  execute_action(wdg, left, right, steps_per_drive_len);
+  return execute_action(wdg, left, right, steps_per_drive_len);
 }
 
 template<typename E1, typename E2>
-void execute_step(const Direction dir, Watchdog& wdg, E1& left, E2& right)
+auto execute_step(const Direction dir, Watchdog& wdg, E1& left, E2& right)
 {
   switch(dir)
   {
-    case Direction::Forward:  execute_drive( wdg, [&] { left.step_forward();  }, [&] { right.step_forward();  } ); return;
-    case Direction::Backward: execute_drive( wdg, [&] { left.step_backward(); }, [&] { right.step_backward(); } ); return;
-    case Direction::Left:     execute_turn ( wdg, [&] { left.step_backward(); }, [&] { right.step_forward();  } ); return;
-    case Direction::Right:    execute_turn ( wdg, [&] { left.step_forward();  }, [&] { right.step_backward(); } ); return;
+    case Direction::Forward:  return execute_drive( wdg, [&] { left.step_forward();  }, [&] { right.step_forward();  } );
+    case Direction::Backward: return execute_drive( wdg, [&] { left.step_backward(); }, [&] { right.step_backward(); } );
+    case Direction::Left:     return execute_turn ( wdg, [&] { left.step_backward(); }, [&] { right.step_forward();  } );
+    case Direction::Right:    return execute_turn ( wdg, [&] { left.step_forward();  }, [&] { right.step_backward(); } );
     case Direction::Stop:
          left.off();
          right.off();
          wdg.reset();
-         return;
+         return false;
   }
+  return false; // never reached
 }
 
 }
@@ -74,14 +79,12 @@ void execute_program(const Direction (&dirs)[N], Watchdog& wdg)
   auto first_run = true;
   for(auto& dir: dirs)
   {
-    if(dir == Direction::Stop)
-      return;
-
     if(first_run)
       first_run = false;
     else
       detail::inter_dir_delay(wdg);
 
-    detail::execute_step(dir, wdg, left, right);
+    if( not detail::execute_step(dir, wdg, left, right) )
+      return;
   }
 }

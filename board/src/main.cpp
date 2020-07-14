43792c3f70e3bed09bced580ac75a3e51f5e6cfd
bartek 'basz' szurgot <git@baszerr.eu>
2020-07-14 23:03:38 +0200

    engines are now a separate entity

--- board/src/main.cpp
+++ board/src/main.cpp
@@ -2,35 +2,34 @@
 #include "Watchdog.hpp"
 #include <util/delay.h>
 #include "Buttons.hpp"
+#include "Engines.hpp"
 
-// LEFT  SEQ: PD0 PD2 PD1 PD3
-// RIGHT SEQ: PB0 PB2 PB1 PB3
-// min  - 15[ms] per step!
-// good - 30[ms] per step!
-
-
-void turn_both(const int (&left)[4], const int (&right)[4])
+enum class Dir
 {
-  for(auto i=0; i<4; ++i)
+  Fwd,
+  Rev
+};
+
+template<typename E>
+void turn_engine(E& e, const Dir dir)
+{
+  switch(dir)
   {
-      PORTD = _BV(left[i]);
-      PORTB = _BV(right[i]);
-      _delay_ms(30);
+    case Dir::Fwd: e.step_forward(); break;
+    case Dir::Rev: e.step_backward(); break;
   }
 }
 
+Engines::Left left;
+Engines::Right right;
 
-void full_stop()
+void turn_steps(const Dir left_dir, const Dir right_dir)
 {
-  PORTD &= ~( _BV(PD0) | _BV(PD1) | _BV(PD2) | _BV(PD3) );
-  PORTB &= ~( _BV(PB0) | _BV(PB1) | _BV(PB2) | _BV(PB3) );
-}
-
-
-void turn_steps(const int (&left)[4], const int (&right)[4])
-{
-  turn_both(left, right);
-  full_stop();
+  turn_engine(left, left_dir);
+  turn_engine(right,right_dir);
+  Engines::step_delay();
+  left.off();
+  right.off();
 }
 
 
@@ -39,61 +38,36 @@ int main(void)
   Buttons::init();
 
   //Watchdog wdt;
-  DDRD |= _BV(PD0) | _BV(PD1) | _BV(PD2) | _BV(PD3);
-  DDRB |= _BV(PB0) | _BV(PB1) | _BV(PB2) | _BV(PB3);
-
-  constexpr int fwd[] = {0,2,1,3};
-  constexpr int rev[] = {fwd[3], fwd[2], fwd[1], fwd[0]};
 
   while(true)
   {
     if( Buttons::up_pressed() )
     {
-      turn_steps(fwd, fwd);
+      turn_steps(Dir::Fwd, Dir::Fwd);
       continue;
     }
     if( Buttons::down_pressed() )
     {
-      turn_steps(rev, rev);
+      turn_steps(Dir::Rev, Dir::Rev);
       continue;
     }
     if( Buttons::left_pressed() )
     {
-      turn_steps(fwd, rev);
+      turn_steps(Dir::Fwd, Dir::Rev);
       continue;
     }
     if( Buttons::right_pressed() )
     {
-      turn_steps(rev, fwd);
+      turn_steps(Dir::Rev, Dir::Fwd);
       continue;
     }
 
     if( Buttons::start_pressed() )
     {
-      turn_steps(fwd, fwd);
-      turn_steps(rev, rev);
+      turn_steps(Dir::Fwd, Dir::Fwd);
+      turn_steps(Dir::Rev, Dir::Rev);
       continue;
     }
   }
 
-  //while(true)
-  for(auto i=0; i<(1*30)/4; ++i)
-  {
-    constexpr int seq[] = {0,2,1,3};
-    for(auto bit: seq)
-    {
-      PORTD = _BV(bit);
-      PORTB = _BV(bit);
-      _delay_ms(30);
-    }
-
-    //_delay_ms(500);
-
-    //wdt.reset();
-    // TODO
-  }
-
-  PORTD = 0x00;
-  PORTB = 0x00;
 }
-
